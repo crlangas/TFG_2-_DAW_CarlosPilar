72ecf75b17a543db0e2c1f0fec9820902e15cd48
crlangas <carlospilarsanz@gmail.com>
2026-05-30 20:21:32 +0200

    minor change 30/5

--- DockerMrFinance/app/mrfinance/client/src/Login.tsx
+++ DockerMrFinance/app/mrfinance/client/src/Login.tsx
@@ -33,36 +33,37 @@ function LoginPage() {
 
   return (
     <>
-      <div id="cuerpo">
-        {/* el class name s es una media query con taiwind para ocultar el lado negro en movil y tablet */}
-        <div className="hidden! lg:flex!">
-          <h1>Hello Thrifty</h1>
-          <h2>¿Estás preparado para mejorar tu economía?</h2>
-        </div>
-        <div className="w-full! lg:w-[50%]!">
-          <p>Create una cuenta o inicia sesión</p>
-          <p>¿Estás preparado para mejorar tu economía de manera gratuita?</p>
-          {view === "selector" && <AuthSelector onSelect={setView} />}
-          {view === "login" && (
-            <LoginForm
-              onBack={() => setView("selector")}
-              onSuccess={handleSuccess}
-            />
-          )}
-          {view === "register" && (
-            <RegisterForm
-              onBack={() => setView("selector")}
-              onSuccess={handleSuccess}
-            />
-          )}
+      <main>
+        <div id="cuerpo">
+          {/* el class name s es una media query con taiwind para ocultar el lado negro en movil y tablet */}
+          <div className="hidden! lg:flex!">
+            <h1>Hello Thrifty</h1>
+            <h2>¿Estás preparado para mejorar tu economía?</h2>
+          </div>
+          <div className="w-full! lg:w-[50%]!">
+            <p>Create una cuenta o inicia sesión</p>
+            <p>¿Estás preparado para mejorar tu economía de manera gratuita?</p>
+            {view === "selector" && <AuthSelector onSelect={setView} />}
+            {view === "login" && (
+              <LoginForm
+                onBack={() => setView("selector")}
+                onSuccess={handleSuccess}
+              />
+            )}
+            {view === "register" && (
+              <RegisterForm
+                onBack={() => setView("selector")}
+                onSuccess={handleSuccess}
+              />
+            )}
 
             <p id="ayuda">
               Si olvidatste tu contraseña o tienes cualquier problema y no eres
               dueno de esta instalación contacta a noreply.mrfinance@gmail.com
             </p>
-
+          </div>
         </div>
-      </div>
+      </main>
     </>
   );
 }
